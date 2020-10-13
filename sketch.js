f83fd78f9e88a048c247998c318a4ddcb138743b
Harshitha-star <72721794+Harshitha-star@users.noreply.github.com>
2020-10-13 15:51:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2, pig1;
+var box3 , lo1, log2,pig2, box4;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(920,320,70,70);
+    box2 = new Box(700,300,70,70);
+    box3 = new Box(700,240,70,70);
+    ground = new Ground(600,height,1200,20)
+    pig1 = new pig(810,350);
+    log1 = new log(810,260,300,PI/2);
+   log4 = new log(760,120,150,PI/7);
+    log5 = new log(870,120,150,-PI/7);
+    box4 = new Box(920, 240, 70,70);
+    pig3 = new pig(810,220);
+    log3 = new log(810,180,300,PI/2);
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    log1.display();
+    log4.display();
+   log5.display();
+    box3.display();
+    box4.display();
+    log3.display();
+    pig3.display();
+}
